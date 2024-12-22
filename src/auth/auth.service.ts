8f1e1c90b4e0fd70bcd60ea88a9be8c9eab835ec
nischal1101 <kaflenk123@gmail.com>
2024-12-22 09:52:37 +0545

    create signup api route

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from "argon2";
 
 @Injectable({})
 export class AuthService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    const hashed = await argon.hash(dto.password);
+    const user = await this.prisma.user.create({
+      data: { email: dto.email, password: hashed },
+    });
+    return user;
+  }
   signin() {
     return "Hello from signin";
-  }
-  signup() {
-    return "Hello from signup";
   }
 }
